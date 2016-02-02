707b2d159f2dce9477239e07b60ce5a65b8e7bdc
kuba <kpx.dev@gmail.com>
2016-02-02 10:10:52 -0800

    setup update

--- bin/setup.js
+++ bin/setup.js
@@ -23,38 +23,45 @@
 		fs.writeFileSync('../config.json', JSON.stringify(data), 'utf-8');
 
 		log('i', 'config.json updated');
+
+		updateSettings();
+	} else {
+		updateUser();
 	}
 
 	// setting up settings
 
-    var update = {
-		setting_id: 1,
-		aws_account_id: process.argv[5],
-		aws_access_key_id: process.argv[6],
-		aws_secret_access_key: process.argv[7],
-		aws_bucket: process.argv[8],
-		aws_region: process.argv[9],
-		aws_s3_domain: 'https://' + process.argv[8] + '.s3.amazonaws.com',
+	function updateSettings(){
 
-		// default preferences
-		device_name: '/dev/sda1',
-		snapshot_id: 'snap-82c0a181',
-		volume_type: 'standard',
-		min_size: 1,
-		max_size: 10,
-		desired_capacity: 1,
+	    var update = {
+			setting_id: 1,
+			aws_account_id: process.argv[5],
+			aws_access_key_id: process.argv[6],
+			aws_secret_access_key: process.argv[7],
+			aws_bucket: process.argv[8],
+			aws_region: process.argv[9],
+			aws_s3_domain: 'https://' + process.argv[8] + '.s3.amazonaws.com',
 
-		created_at: new Date()
-	}
-	db.setting.findOneAndUpdate({ setting_id: 1 }, update, { upsert: true, new: true }, function(err, data){
-		if(err){
-			log('e', 'unable to save settings');
-		} else {
-			log('i', 'settings updated');
+			// default preferences
+			device_name: '/dev/sda1',
+			snapshot_id: 'snap-82c0a181',
+			volume_type: 'standard',
+			min_size: 1,
+			max_size: 10,
+			desired_capacity: 1,
 
-			updateUser();
+			created_at: new Date()
 		}
-	});
+		db.setting.findOneAndUpdate({ setting_id: 1 }, update, { upsert: true, new: true }, function(err, data){
+			if(err){
+				log('e', 'unable to save settings');
+			} else {
+				log('i', 'settings updated');
+
+				updateUser();
+			}
+		});
+	};
 
 	// setting up password for default user
 	
